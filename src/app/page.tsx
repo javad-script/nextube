f6a61ecc24ff9f091cc72de1ddce37b069f87a87
Djawad <dev.javad.mousavian@gmail.com>
2026-05-31 20:04:37 +0330

    feat: install shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export default function Page() {
-  return <div>hello world</div>;
+  return (
+    <div>
+      <Button variant={"default"}>hi</Button>
+    </div>
+  );
 }
