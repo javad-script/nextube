44ab18f9d9e991247478c3672d1d233426dffc9e
Djawad <dev.javad.mousavian@gmail.com>
2026-05-31 23:12:56 +0330

    feat: install redis and create redis client

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -8,6 +8,7 @@ export const env = createEnv({
    */
   server: {
     DATABASE_URL: z.string().url(),
+    REDIS_URL: z.string().url(),
     NODE_ENV: z.enum(["development", "test", "production"]).default("development"),
   },
 
@@ -26,6 +27,7 @@ export const env = createEnv({
    */
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
+    REDIS_URL: process.env.REDIS_URL,
     NODE_ENV: process.env.NODE_ENV,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
---
+++ src/server/redis.ts
@@ -0,0 +1,21 @@
+import { createClient } from "redis";
+
+const globalForRedis = globalThis as unknown as {
+  redis: ReturnType<typeof createClient> | undefined;
+};
+
+export const redis =
+  globalForRedis.redis ??
+  createClient({
+    url: process.env.REDIS_URL,
+  });
+
+globalForRedis.redis ??= redis;
+
+redis.on("error", (err) => {
+  console.error("Redis Client Error", err);
+});
+
+if (!redis.isOpen) {
+  await redis.connect();
+}
