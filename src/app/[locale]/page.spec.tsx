a8ee029efaf6e432697a938be9031945a9b1a5e5
Djawad <dev.javad.mousavian@gmail.com>
2026-06-01 00:58:41 +0330

    feat: install and config vitest and react testing library

---
+++ src/app/[locale]/page.spec.tsx
@@ -0,0 +1,33 @@
+import Page from "@/app/[locale]/page";
+import { redis } from "@/server/redis";
+import { render, screen } from "@testing-library/react";
+import { expect, it, vi } from "vitest";
+
+import { getTranslations } from "next-intl/server";
+
+vi.mock("@/server/redis", () => ({
+  redis: {
+    get: vi.fn(),
+  },
+}));
+
+vi.mock("next-intl/server", () => ({
+  getTranslations: vi.fn(),
+}));
+
+it("renders translated title with redis value", async () => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  (redis.get as any).mockResolvedValue("Javad");
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  (getTranslations as any).mockResolvedValue((key: string) => {
+    const dict: Record<string, string> = {
+      title: "Hello",
+    };
+    return dict[key];
+  });
+
+  render(await Page());
+
+  expect(screen.getByRole("button", { name: "Hello Javad" })).toBeDefined();
+});
