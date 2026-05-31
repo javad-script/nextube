f201f91554ad2bc7d16eecc5858b455be2e94325
Djawad <dev.javad.mousavian@gmail.com>
2026-05-31 23:12:56 +0330

    feat: install redis and create redis client

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,12 +1,15 @@
+import { redis } from "@/server/redis";
+
 import { getTranslations } from "next-intl/server";
 
 import { Button } from "@/components/ui/button";
 
 export default async function Page() {
+  const name = await redis.get("name");
   const t = await getTranslations("homePage");
   return (
     <div>
-      <Button variant={"default"}>{t("title")}</Button>
+      <Button variant={"default"}>{`${t("title")} ${name}`}</Button>
     </div>
   );
 }
